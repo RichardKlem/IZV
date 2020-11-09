6220087a42a71567c22e6cfb33017e1301f135c7
Richard Klem <rklem@codasip.com>
2020-11-09 19:24:57 +0100

    Refactor downloading, basic parsing

--- download.py
+++ download.py
@@ -3,11 +3,12 @@ import os
 import re
 import sys
 import urllib.request
+from zipfile import ZipFile
 
 import requests
 from bs4 import BeautifulSoup
 
-from constants import REGIONS
+from constants import DATA_FILES, DATA_WEB_ROOT, NUMS_REGIONS, REGIONS_NUMS
 
 MIN_PYTHON = (3, 8)
 if sys.version_info < MIN_PYTHON:
@@ -26,11 +27,11 @@ class DataDownloader:
         """
         Proper files are downloaded into given folder. Returns nothing.
         :param url: Source of files to download.
-        :param folder:Path to the output directory.
+        :param folder: Name of the directory with data.
         :param cache_filename:
         """
         self.url = url
-        self.folder = folder
+        self.folder = folder if folder.endswith(os.path.sep) else folder + os.path.sep
         self.cash_filename = cache_filename
 
     def download_data(self):
@@ -45,17 +46,13 @@ class DataDownloader:
         downloaded_files = os.walk(self.folder).__next__()[2]
 
         # Get data file names from the given url
-        req = requests.get(self.url, headers={"User-Agent": ""})
-        soup = BeautifulSoup(req.text, "html.parser")
-        pages = soup.findAll("a", attrs={"href": re.compile(r"data/datagis(?:-rok-)?\d{4}.zip$")})
-        zipfilenames = [pages[i].attrs["href"] for i in range(len(pages))]
         [
             urllib.request.urlretrieve(
-                self.url + zipfilenames[i],
-                self.folder + os.path.sep + zipfilenames[i][5:],
+                self.url + DATA_WEB_ROOT + DATA_FILES[year],
+                self.folder + DATA_FILES[year],
             )
-            for i in range(len(zipfilenames))
-            if zipfilenames[i][5:] not in downloaded_files
+            for year in DATA_FILES.keys()
+            if DATA_FILES[year] not in downloaded_files
         ]
 
     def parse_region_data(self, region):
@@ -63,16 +60,26 @@ class DataDownloader:
         Downloads all data files for the given region, if they are not already downloaded.
         Then for the given region, function parses data. The returned structure looks like this:
             tuple(list[str], list[np.ndarray]) where
-                the list of string equals to the list of region three-char shortcuts,
+                the list of string equals to the list of the header elements,
                 the list of NumPy arrays equals to the list of data.
             The length of the both list is the same. Also shape property of all NumPy arrays is
             the same.
         :param region: The three-char shortcut of the region name.
         :return: Tuple of two lists, described above.
         """
-        ...
+        # Download data
+        downloader.download_data()
 
-    def get_list(self, regions=REGIONS.keys()):
+        zipfile = ZipFile(self.folder + "datagis2016.zip", "r")
+        for file in zipfile.filelist:
+            try:
+                filename = int(file.filename[:2])
+            except ValueError:
+                continue
+            if filename == REGIONS_NUMS[region]:
+                print(zipfile.read(file.filename).decode('1250').split(os.linesep)[0])
+
+    def get_list(self, regions=REGIONS_NUMS.keys()):
         """
         :param regions: A list of three-char shortcuts of regions to process. If None,
         all regions are processed.
@@ -86,4 +93,4 @@ if __name__ == "__main__":
     # for region, data in (region, data):
     #    print(region, data.shape)
     downloader = DataDownloader()
-    downloader.download_data()
+    downloader.parse_region_data("PHA")
