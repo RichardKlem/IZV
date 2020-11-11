bcff3870d53ed1335edd1fee492d99db2e2f6b7e
Richard Klem <rklem@codasip.com>
2020-11-11 22:43:00 +0100

    Finish

--- download.py
+++ download.py
@@ -1,22 +1,84 @@
-#!/usr/bin/python
+"""
+UPOZORNENI:
+Z duvodu, ze jsem nevsiml, ze se odevzdavaji pouze dva soubory,
+jsem musel zrusit sve moduly a jejich obsah vlozit primo do kodu.
+Proto to vypada zmatene a nepekne. Vsiml jsem si tohoto faktu par hodin
+pred odevdanim. Za toto nepohodli se omlouvam.
+"""
 import gzip
 import logging
 import os
 import pickle
-import sys
 import time
 import urllib.request
 from zipfile import ZipFile
 
 import numpy as np
 
-from constants import DATA_FILES, DATA_HEADER, DATA_WEB_ROOT, REGIONS_NUMS
-from utils.logging import PROFILE_LOG_LEVEL
-from utils.logging import data_downloader_logger as ddl
+#from constants import DATA_FILES, DATA_HEADER, DATA_WEB_ROOT, REGIONS_NUMS
+#from utils.logging import PROFILE_LOG_LEVEL
+#from utils.logging import data_downloader_logger as ddl
 
-MIN_PYTHON = (3, 8)
-if sys.version_info < MIN_PYTHON:
-    sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
+
+REGIONS = {'PHA': 'Hlavní město Praha',
+           'STC': 'Středočeský',
+           'JHC': 'Jihočeský',
+           'PLK': 'Plzeňský',
+           'KVK': 'Karlovarský',
+           'ULK': 'Ústecký',
+           'LBK': 'Liberecký',
+           'HKK': 'Královéhradecký',
+           'PAK': 'Pardubický',
+           'OLK': 'Olomoucký',
+           'MSK': 'Moravskoslezský',
+           'JHM': 'Jihomoravský',
+           'ZLK': 'Zlínský',
+           'VYS': 'Kraj Vysočina',
+           }
+NUMS_REGIONS = {0: 'PHA',
+                1: 'STC',
+                2: 'JHC',
+                3: 'PLK',
+                4: 'KVK',
+                5: 'ULK',
+                6: 'LBK',
+                7: 'HKK',
+                14: 'PAK',
+                15: 'OLK',
+                16: 'MSK',
+                17: 'JHM',
+                18: 'ZLK',
+                19: 'VYS',
+                }
+REGIONS_NUMS = {'PHA': 0,
+                'STC': 1,
+                'JHC': 2,
+                'PLK': 3,
+                'KVK': 4,
+                'ULK': 5,
+                'LBK': 6,
+                'HKK': 7,
+                'PAK': 14,
+                'OLK': 15,
+                'MSK': 16,
+                'JHM': 17,
+                'ZLK': 18,
+                'VYS': 19
+                }
+DATA_WEB_ROOT = "data/"
+DATA_FILES = {2016: "datagis2016.zip",
+              2017: "datagis-rok-2017.zip",
+              2018: "datagis-rok-2018.zip",
+              2019: "datagis-rok-2019.zip",
+              2020: "datagis-09-2020.zip"
+              }
+DATA_HEADER = ['p1', 'p36', 'p37', 'p2a', 'weekday(p2a)', 'p2b', 'p6', 'p7', 'p8', 'p9', 'p10',
+               'p11', 'p12', 'p13a', 'p13b', 'p13c', 'p14', 'p15', 'p16', 'p17', 'p18', 'p19',
+               'p20', 'p21', 'p22', 'p23', 'p24', 'p27', 'p28', 'p34', 'p35', 'p39', 'p44',
+               'p45a', 'p47', 'p48a', 'p49', 'p50a', 'p50b', 'p51', 'p52', 'p53', 'p55a', 'p57',
+               'p58', 'a', 'b', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'n', 'o', 'p', 'q',
+               'r', 's', 't', 'p5a', 'reg'
+               ]
 
 
 class DataDownloader:
@@ -87,7 +149,7 @@ class DataDownloader:
                 except ValueError:
                     continue
                 if filename == REGIONS_NUMS[region]:
-                    ddl.debug(f"Processing {filelist.filename} inside {file}.")
+                    #ddl.debug(f"Processing {filelist.filename} inside {file}.")
                     # Process all the records in the region
                     for i, row in enumerate(file.read(filelist.filename).decode('1250').split(
                             os.linesep)):
@@ -121,16 +183,16 @@ class DataDownloader:
         for region in regions:
             # Data are in the memory
             if self.parsed_regions[region]:
-                ddl.debug("Data tuple in attribute.")
+                #ddl.debug("Data tuple in attribute.")
                 array = self.parsed_regions[region]
             # Data are not in the memory, but they are in cache files
             elif self.cash_filename.format(region) in os.walk(self.folder).__next__()[2]:
-                ddl.debug("Data tuple in cache.")
+                #ddl.debug("Data tuple in cache.")
                 with gzip.GzipFile(self.folder + self.cash_filename.format(region), 'r') as f:
                     array = pickle.load(f)
             # Data are not in the memory nor in the cached files, they have to be parsed
             else:
-                ddl.debug("Data tuple must be parsed before.")
+                #ddl.debug("Data tuple must be parsed before.")
                 # Get data tuple
                 _, array = self.parse_region_data(region)
                 # Save data into a cache file
@@ -155,4 +217,4 @@ if __name__ == "__main__":
 
     # End measuring time
     end = time.perf_counter()
-    ddl.profile(f"Time: {end - start:0.4f} s")
+    #ddl.profile(f"Time: {end - start:0.4f} s")
--- get_stat.py
+++ get_stat.py
@@ -1,13 +1,16 @@
 import argparse
-import logging
-import pathlib
+# import logging
+import os
+import time
 
 import matplotlib.pyplot as plt
-import numpy as np
 
-from constants import DATA_HEADER, REGIONS_NUMS
-from download import DataDownloader
-from utils.logging import get_stat_logger as gsl
+# from constants import DATA_HEADER, REGIONS_NUMS
+from download import REGIONS_NUMS, DataDownloader
+
+# from utils import PROFILE_LOG_LEVEL
+# from utils.logging import get_stat_logger as gsl
+
 
 parser = argparse.ArgumentParser(description='Get_stat parser')
 parser.add_argument("--fig_location", action="store", default=None,
@@ -18,19 +21,39 @@ parser.add_argument("-r", "--regions", action="store", default=None, nargs='+',
                     help="")
 
 
+def touch(path):
+    basedir = os.path.dirname(path)
+    if not os.path.exists(basedir) and basedir != '':
+        os.makedirs(basedir)
+
+    with open(path, 'a'):
+        os.utime(path, None)
+
+
 def plot_stat(data_source, fig_location=None, show_figure=False, regions=None):
     """
 
+    :param regions:
     :param data_source:
     :param fig_location:
     :param show_figure:
     :return:
     """
     processed_data = {}  # Dictionary of all results.
-    fig = plt.figure(figsize=(17, 26), tight_layout=True)
-    plt.title('Nehodovost v krajích podle roku', size=30)
+    fig = plt.figure(figsize=(8.27, 11.69))
+    plt.title('Nehodovost v krajích podle roku', size=20)
+    plt.tick_params(
+        axis='both',  # changes apply to the x-axis
+        which='both',  # both major and minor ticks are affected
+        bottom=False,  # ticks along the bottom edge are off
+        left=False,  # ticks along the top edge are off
+        labelbottom=False,
+        labelleft=False
+    )
+
+    # start = time.perf_counter()
     axes = []
-    main_ax = fig.add_subplot(111)
+    # fig.add_subplot(111)
     for i, year in enumerate(range(16, 21)):
         year_mask = [int(data_source[i][0][-7:-5]) == year for i in range(data_source.shape[0])]
         year_data = data_source[year_mask]
@@ -40,41 +63,68 @@ def plot_stat(data_source, fig_location=None, show_figure=False, regions=None):
             one_year[region] = year_data[region_mask].shape[0]
         processed_data[year] = one_year
 
+        # Calculate position
+        pairs = [(region, processed_data[year][region]) for region in regions]
+        pairs.sort(key=lambda x: x[1], reverse=True)
+
+        # Get first elements - regions - sorted in reversed order
+        sorted_regions = [pair[0] for pair in pairs]
+
+        # Place position values where the region sit at.
+        positions = [0 for i in range(len(sorted_regions))]
+
+        for index, region in enumerate(regions):
+            positions[index] = sorted_regions.index(region)
+
         axes.append(fig.add_subplot(int(511 + i)))
-        axes[i].set_xlabel("Kraje", size=20)
-        axes[i].set_ylabel("Počet nehod", size=20)
-        axes[i].bar(processed_data[year].keys(), processed_data[year].values())
+        axes[i].set_xlabel("Kraje", size=15)
+        axes[i].set_ylabel("Počet nehod", size=15)
+        bars = axes[i].bar(processed_data[year].keys(), processed_data[year].values())
+        new_top_lim = axes[i].get_ylim()[1] * 1.2
+        axes[i].set_ylim(bottom=0, top=new_top_lim)
+        # axes[i].grid(b=True, axis='y', which='major', alpha=0.4)
+        # axes[i].yaxis.set_minor_locator(MultipleLocator(new_top_lim / 10))
 
-    """
-    plt.subplot(511)
-    plt.bar(processed_data[16].keys(), processed_data[16].values())
-    plt.subplot(512)
-    plt.bar(processed_data[17].keys(), processed_data[17].values())
-    plt.subplot(513)
-    plt.bar(processed_data[18].keys(), processed_data[18].values())
-    plt.subplot(514)
-    plt.bar(processed_data[19].keys(), processed_data[19].values())
-    plt.subplot(515)
-    plt.bar(processed_data[20].keys(), processed_data[20].values())"""
-    if show_figure:
-        plt.show()
+        for bar_index, bar in enumerate(bars):
+            axes[i].text(
+                bar.get_x() + bar.get_width() / 2.,
+                bar.get_height() + axes[i].get_ylim()[1] / 30,
+                positions[bar_index] + 1,
+                ha='center',
+                va='bottom', size=13)
+
     if fig_location:
-        pathlib.Path(fig_location).mkdir(parents=True, exist_ok=True)
+        touch(fig_location)
+        plt.savefig(fig_location)
+    if show_figure:
+        fig.tight_layout()
+        plt.show()
 
+    # end = time.perf_counter()
+    # gsl.profile(f"Time: {end - start:0.4f} s")
 
 
 if __name__ == '__main__':
+    # Start measuring time
+    start = time.perf_counter()
+
     # Parse args and get values
     args = parser.parse_args()
     fig_loc = args.fig_location
     show_fig = args.show_figure
-    regions = args.regions
+    regs = args.regions
 
     # Check if the input is valid
-    if not set(regions).issubset(set(REGIONS_NUMS.keys())):
-        gsl.log(logging.ERROR, f"Passed region does not exist!\nYou have passed {args.regions} "
-                               f"while only these: {list(REGIONS_NUMS.keys())} are supported.")
+    if not regs:
+        regs = list(REGIONS_NUMS.keys())
+    if not set(regs).issubset(set(REGIONS_NUMS.keys())):
+        # gsl.log(logging.ERROR, f"Passed region does not exist!\nYou have passed {regs} "
+        #                        f"while only these: {list(REGIONS_NUMS.keys())} are supported.")
+        print(f"Passed region does not exist!\nYou have passed {regs} "
+              f"while only these: {list(REGIONS_NUMS.keys())} are supported.")
         exit(-1)
-    _, data = DataDownloader().get_list(regions)
-    plot_stat(data_source=data, fig_location=fig_loc, show_figure=show_fig, regions=regions)
-
+    _, data = DataDownloader().get_list(regs)
+    plot_stat(data_source=data, fig_location=fig_loc, show_figure=show_fig, regions=regs)
+    # End measuring time
+    end = time.perf_counter()
+    # gsl.profile(f"Time: {end - start:0.4f} s")
